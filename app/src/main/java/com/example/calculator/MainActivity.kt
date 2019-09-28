1e7f08769e6050f04c4323f8800f7727dec07694
Chris Johnson <cljohnson4343@gmail.com>
2019-09-28 14:39:08 -0500

    Set up basic portrait calc UI.

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,11 +2,22 @@ package com.example.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onClick(view: View) {
+        when(view.id) {
+            R.id.btnOne -> appendChar()
+        }
+    }
+
+    fun appendChar() {
+
     }
 }
