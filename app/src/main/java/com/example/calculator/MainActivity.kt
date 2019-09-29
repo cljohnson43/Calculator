16c323e21f0508ebcd2da4e3989be70990f2f66c
Chris Johnson <cljohnson4343@gmail.com>
2019-09-29 14:18:03 -0500

    add auto close parentheses feature. Formatting changes.

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -4,12 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
+import net.objecthunter.exp4j.Expression
+import net.objecthunter.exp4j.ExpressionBuilder
 
 const val MULTIPLY_CHAR = '*'
 const val DIVIDE_CHAR = '/'
 const val ADD_CHAR = '+'
 const val MODULO_CHAR = '%'
-const val SUBTRACT_CHAR = '\u2212'
+const val SUBTRACT_CHAR = '-'
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,34 +21,38 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun onClick(view: View) {
-        tvInput.text = getStringExpression(tvInput.text.toString(), view.id)
+        val newExpr = getNewExpression(tvInput.text.toString(), view.id)
+        tvResults.text = evaluate(autoCloseParentheses(newExpr))
+        tvInput.text = newExpr
     }
 
-    fun getStringExpression(str: String, btnID: Int): String {
+    fun getNewExpression(str: String, btnID: Int): String {
+        var newExp: String = ""
         when (btnID) {
-            R.id.btnOne -> return addDigit(str, "1")
-            R.id.btnTwo -> return addDigit(str, "2")
-            R.id.btnThree -> return addDigit(str, "3")
-            R.id.btnFour -> return addDigit(str, "4")
-            R.id.btnFive -> return addDigit(str, "5")
-            R.id.btnSix -> return addDigit(str, "6")
-            R.id.btnSeven -> return addDigit(str, "7")
-            R.id.btnEight -> return addDigit(str, "8")
-            R.id.btnNine -> return addDigit(str, "9")
-            R.id.btnZero -> return addDigit(str, "0")
-            R.id.btnClear -> return ""
-            R.id.btnParentheses -> return addParentheses(str)
-            R.id.btnPercent -> return addOperation(str, MODULO_CHAR)
-            R.id.btnDivide -> return addOperation(str, DIVIDE_CHAR)
-            R.id.btnMultiply -> return addOperation(str, MULTIPLY_CHAR)
-            R.id.btnAdd -> return addOperation(str, ADD_CHAR)
-            R.id.btnSubtract -> return addOperation(str, SUBTRACT_CHAR)
-            R.id.btnDecimal -> return addDecimal(str)
-            R.id.btnSign -> return changeSign(str)
-            R.id.btnBackspace -> return if (str.length == 0) str else "${str.dropLast(1)}"
-            R.id.btnEqual -> return ""//TODO
+            R.id.btnOne -> newExp = addDigit(str, "1")
+            R.id.btnTwo -> newExp = addDigit(str, "2")
+            R.id.btnThree -> newExp = addDigit(str, "3")
+            R.id.btnFour -> newExp = addDigit(str, "4")
+            R.id.btnFive -> newExp = addDigit(str, "5")
+            R.id.btnSix -> newExp = addDigit(str, "6")
+            R.id.btnSeven -> newExp = addDigit(str, "7")
+            R.id.btnEight -> newExp = addDigit(str, "8")
+            R.id.btnNine -> newExp = addDigit(str, "9")
+            R.id.btnZero -> newExp = addDigit(str, "0") // TODO case where number starts w/ zero
+            R.id.btnClear -> newExp = ""
+            R.id.btnParentheses -> newExp = addParentheses(str)
+            R.id.btnPercent -> newExp = addOperation(str, MODULO_CHAR)
+            R.id.btnDivide -> newExp = addOperation(str, DIVIDE_CHAR)
+            R.id.btnMultiply -> newExp = addOperation(str, MULTIPLY_CHAR)
+            R.id.btnAdd -> newExp = addOperation(str, ADD_CHAR)
+            R.id.btnSubtract -> newExp = addOperation(str, SUBTRACT_CHAR)
+            R.id.btnDecimal -> newExp = addDecimal(str)
+            R.id.btnSign -> newExp = changeSign(str)
+            R.id.btnBackspace -> newExp = if (str.length == 0) str else "${str.dropLast(1)}"
+            R.id.btnEqual -> newExp = evaluate(autoCloseParentheses(str))
         }
-        return str
+
+        return newExp
     }
 
     fun addDigit(str: String, digit: String): String {
@@ -136,19 +142,36 @@ class MainActivity : AppCompatActivity() {
         return listOf()
     }
 
-    /*
-    fun evaluate(str: String): Unit {
-        var result: dynamic
-
+    fun evaluate(str: String): String {
+        val exp: Expression?
+        var results: String
         try {
-            eval(str)
+            exp = ExpressionBuilder(str).build()
+            results = exp.evaluate().toString()
 
         } catch (e: Throwable) {
-            tvResults.text = e.message
-           // TODO this section
+            results = ""
         }
+
+        return formatResults(results)
     }
 
-     */
+    fun formatResults(str: String): String {
+        if (str.length < 3) return str
+        val lastTwo = str.takeLast(2)
+        if (lastTwo.equals(".0")) return str.dropLast(2)
+        return str
+    }
+
+    fun autoCloseParentheses(str: String): String {
+        val numOpenParentheses = str.count{it == '('}
+        val numClosedParentheses = str.count{it == ')'}
+        if (numOpenParentheses > numClosedParentheses) {
+            val diff = numOpenParentheses - numClosedParentheses
+            val closeExpression = ")".repeat(diff)
+            return str.plus(closeExpression)
+        }
+        return str
+    }
 
 }
